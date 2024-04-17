f2fb19ef572282c4c2605d51220275ca96744ef8
Victor Colome <victorcolomec@gmail.com>
2024-04-17 15:36:22 +0200

    Update audit sender

--- audit_sender.py
+++ audit_sender.py
@@ -10,8 +10,10 @@ class AuditSender(MessagingHandler):
 
     def __init__(self, audit_info: AuditInfo, config: dict, logger: logging.Logger) -> None:
         """
-        Constructor
-        :param config: configuration settings
+        Constructor.
+        :param audit_info: AuditInfo class containing all the information to be sent and the function to convert to XML.
+        :param config: configuration settings.
+        :param logger: logger.
         """
         super(AuditSender, self).__init__()
         self.url = config["activemq_url"]
@@ -21,25 +23,39 @@ class AuditSender(MessagingHandler):
         self.body = audit_info.as_xml()
 
     def on_start(self, event: Event) -> None:
-        self.logger.info("connecting ...")
-        # https://qpid.apache.org/releases/qpid-proton-0.32.0/proton/python/docs/proton.html#proton.SSLDomain
-        ssl_domain = SSLDomain(SSLDomain.MODE_CLIENT)  # Is this mode?
+        """
+        Establish QPID Proton connectivity to the ActiveMQ server using SSL.
+        Check SSL configuration: https://qpid.apache.org/releases/qpid-proton-0.32.0/proton/python/docs/proton.html#proton.SSLDomain
+        :param event: QPID Proton notification of a state change in the protocol engine.
+        """
+        self.logger.info("Establishing SSL connectivity with AMQ")
+        ssl_domain = SSLDomain(SSLDomain.MODE_CLIENT)
         ssl_domain.set_credentials(cert_file=self.ca_cert_file, key_file="", password=None)
         conn = event.container.connect(self.url, ssl_domain=ssl_domain)
-        event.container.create_sender(conn)  # Añadir self.host como parametro tmbn?
+        event.container.create_sender(conn, self.queue)
+        self.logger.info("Connection established")
 
     def on_sendable(self, event: Event) -> None:
-        # Async send message
+        """
+        Send a message to the broker queue asynchronously.
+        :param event: QPID Proton notification of a state change in the protocol engine.
+        """
         while event.sender.credit:
             self.logger.info(f"Send: {self.body}")
             msg = Message(self.body)
             event.sender.send(msg)
 
     def on_accepted(self, event: Event) -> None:
-        # Message received
-        self.logger.info("accepted")
+        """
+        Message received correctly.
+        :param event: QPID Proton notification of a state change in the protocol engine.
+        """
+        self.logger.info("Accepted message")
         event.connection.close()
 
     def on_disconnected(self, event: Event) -> None:
-        # Connection closed
-        self.logger.info("disconnected")
+        """
+        Connection closed event.
+        :param event: QPID Proton notification of a state change in the protocol engine.
+        """
+        self.logger.info("Disconnected")
