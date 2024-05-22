10ff8ab0083b4cae91f9c3c95836e4cbe26f34ec
Oscar Martinez Rubi <omrubi@gmail.com>
2024-05-22 15:22:11 +0200

    Update audit_info.py
    escape chars that need it for XML (only paths, audit_path and errorMessage)

--- audit_info.py
+++ audit_info.py
@@ -23,6 +23,24 @@ class AuditInfo:
         self.paths = paths
         self.audit_path = audit_path
 
+    def escape_field(self, field: str) -> str: 
+        if field:
+            ichars = {
+'<' : '&lt',
+'&' : '&amp',
+'>' : '&gt',
+"'" : '&apos',
+'"' : '&quot' 
+}
+            nfield=''
+            for c in field:
+                if c in ichars:
+                    nfield += ichars[c]
+                else:
+                    nfield += c
+            field = nfield
+        return field
+
     def as_xml(self) -> str:
         """
         Convert class into specific audit XML string.
@@ -43,9 +61,9 @@ class AuditInfo:
          <action>
             <actionType>{self.workflow_state}</actionType>
             <additionalInfo name="jobId">{self.job_id}</additionalInfo>
-            <additionalInfo name="errorMessage">{self.error_message}</additionalInfo>
-            <additionalInfo name="paths">{','.join(self.paths)}</additionalInfo>
-            <additionalInfo name="audit_path">{self.audit_path}</additionalInfo>
+            <additionalInfo name="errorMessage">{self.escape_field(self.error_message})</additionalInfo>
+            <additionalInfo name="paths">{self.escape_field(','.join(self.paths))}</additionalInfo>
+            <additionalInfo name="audit_path">{self.escape_field(self.audit_path)}</additionalInfo>
             <timestamp>{self.workflow_timestamp}</timestamp>
         </action>
     </auditEvent>
